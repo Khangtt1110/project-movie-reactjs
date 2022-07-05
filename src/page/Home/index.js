cb25b0d64ea69f13782784226a01fd3d30a8e659
KhangTT <Khangtt@rikkeisoft.com>
2022-07-05 14:52:55 +0700

    Commit initial setup

---
+++ src/page/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '~/layout/Header';
+import Footer from '~/layout/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <div>Body</div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
